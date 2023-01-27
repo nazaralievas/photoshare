5c20a64f37434bdd832ce340a182e16ceaf186d8
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-28 04:30:33 +0600

    image uploading function added

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 
@@ -15,4 +15,26 @@ def photoView(request, pk):
 
 
 def addPhoto(request):
-    return render(request, 'photos/add.html')
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+        
+        photo = Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image
+        )
+
+        return redirect('gallery')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
