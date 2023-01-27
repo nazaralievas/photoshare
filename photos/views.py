7de4b1e06ca8739569d12bb2f5b2ce30127150d4
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-28 04:40:53 +0600

    filtering by category added

--- photos/views.py
+++ photos/views.py
@@ -3,8 +3,13 @@ from .models import *
 
 
 def gallery(request):
+    category = request.GET.get('category')
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
     categories = Category.objects.all()
-    photos = Photo.objects.all()
     context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html', context)
 
